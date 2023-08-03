9219f8783d16794ca4fb234e527b0f1a0457fe9a
higruo-seungjin <xeungjin@gmail.com>
2023-08-03 16:07:56 +0900

    ~204page

--- ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -3,11 +3,17 @@ package org.zerock.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
 import org.zerock.mapper.BoardMapper;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
+@Service
+@AllArgsConstructor
 public class BoardServiceImpl implements BoardService{
 
 	@Setter(onMethod_=@Autowired)
@@ -42,4 +48,6 @@ public class BoardServiceImpl implements BoardService{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 }
